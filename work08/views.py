3a24746c259db6dd247ebcd7744ab30285f2b7df
Bosak777 <masafujurasaki1126@gmail.com>
2025-11-05 12:04:36 +0900

    a

--- work08/views.py
+++ work08/views.py
@@ -10,28 +10,30 @@
 # Create your views here.
 import mysql.connector
 from django.shortcuts import render
+import os
+
 # from .models import Memo
 
 
 def memo(request):
     conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='bosaklong',
-        database='work08'
+        host=os.environ.get("DB_HOST", "mysql.railway.internal"),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD", ""),
+        database=os.environ.get("DB_NAME", "railway"),
+        port=os.environ.get("DB_PORT", 3306),
     )
     cursor = conn.cursor(dictionary=True)
 
     # メモタイトルを取得
-    cursor.execute("select id, title from memo order by id desc")
+    cursor.execute("SELECT id, title FROM memo ORDER BY id DESC")
     memos = cursor.fetchall()
-
-    print(memos)
 
     cursor.close()
     conn.close()
-    # index_top.hmtlにデータを渡す
-    return render(request, 'work08/index_top.html', {"memos": memos})
+
+    # index_top.html にデータを渡す
+    return render(request, "work08/index_top.html", {"memos": memos})
 
 
 def add_memo(request):
@@ -42,21 +44,19 @@ def add_memo(request):
         content = request.POST.get("content")
 
         conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='bosaklong',
-            database='work08'
+            host="localhost", user="root", password="bosaklong", database="work08"
         )
         # カーソル作成
         cursor = conn.cursor()
         # データを挿入
-        query = 'insert into memo (title, content) values (%s, %s)'
+        query = "insert into memo (title, content) values (%s, %s)"
         cursor.execute(query, (title, content))
         conn.commit()
         cursor.close()
         conn.close()
         result = "保存しました"
-    return render(request, "work08/add_memo.html", {'result': result})
+    return render(request, "work08/add_memo.html", {"result": result})
+
 
 #  データベース;work08
 #  テーブル:memo
